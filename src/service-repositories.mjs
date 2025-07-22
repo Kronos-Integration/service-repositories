56e596f334d4b4d8f5aecd0d8f1de039ff1149f7
Markus Felten <Markus.Felten@gmx.de>
2025-07-22 18:52:21 +0100

    fix: get rid of mergeAttributeDefinitions

--- src/service-repositories.mjs
+++ src/service-repositories.mjs
@@ -1,19 +1,19 @@
 import AggregationProvider from "aggregation-repository-provider";
 import { Service } from "@kronos-integration/service";
-import { prepareAttributesDefinitions, mergeAttributeDefinitions, default_attribute } from "pacc";
+import { prepareAttributesDefinitions, default_attribute } from "pacc";
 
 /**
  * Provide repositories.
  */
 export class ServiceRepositories extends Service {
-  static attributes = mergeAttributeDefinitions(
-    prepareAttributesDefinitions({
+  static attributes = prepareAttributesDefinitions(
+    {
       providers: {
         ...default_attribute,
         description: "list of providers to be accessed",
         needsRestart: true
       }
-    }),
+    },
     Service.attributes
   );
 
